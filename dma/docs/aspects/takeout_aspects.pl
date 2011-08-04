10fe39ec7db586df0c18c1102dd76b7872f0df73
Sean Heber <sean@iconfactory.com>
2011-08-04 15:19:30 -0500

    I dunno

--- dma/docs/aspects/takeout_aspects.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(library(aspects), [aspect/3, aspect_declaration/1]).
-
-:- use_module(library(list_utils), [takeout/3]).
-:- aspect_declaration(use_module(library(list_utils), [takeout/3])).
-:- ensure_loaded(library(lists)).
-:- aspect_declaration(ensure_loaded(library(lists))).
-
-:- aspect(around, list_utils:takeout(Elt, L0, L1), delete(L0, Elt, L1)).
